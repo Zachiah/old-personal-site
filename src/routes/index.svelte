656bad9fdf8b17dd5658b6b6e28f8f6e79840303
Zachiah Sawyer <zachiahsawyer1@gmail.com>
2020-08-23 20:09:00 -0600

    add links to stuff i know

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,22 +10,22 @@
 
 <h2 class="title is-2 mt-4 mb-1">Technologies I know/am learning</h2>
 <ul>
-    <li><abbr title="HyperText Markup Language">HTML</abbr></li>
-    <li><abbr title="Cascading Style Sheets">CSS</abbr></li>
-    <li>Bootstrap 4</li>
-    <li>Bulma</li>
-    <li>Materialize CSS</li>
-    <li>JavaScript</li>
-    <li>Vue.js</li>
-    <li>Svelte.js</li>
-    <li>Sapper</li>
-    <li>PHP</li>
-    <li>Laravel</li>
-    <li>Ruby</li>
-    <li>Ruby on Rails</li>
-    <li>Python</li>
-    <li>Flask</li>
-    <li>Crystal</li>
+    <li><a href="https://developer.mozilla.org/en-US/docs/Web/HTML"><abbr title="HyperText Markup Language">HTML</abbr></a></li>
+    <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS"><abbr title="Cascading Style Sheets">CSS</abbr></a></li>
+    <li><a href="https://getbootstrap.com/">Bootstrap 4</a></li>
+    <li><a href="https://bulma.io">Bulma</a></li>
+    <li><a href="https://materializecss.com">Materialize CSS</a></li>
+    <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/">JavaScript</a></li>
+    <li><a href="https://vuejs.org/">Vue.js</a></li>
+    <li><a href="https://svelte.dev/">Svelte.js</a></li>
+    <li><a href="https://sapper.svelte.dev/">Sapper</a></li>
+    <li><a href="https://www.php.net/">PHP</a></li>
+    <li><a href="https://laravel.com/">Laravel</a></li>
+    <li><a href="https://ruby-lang.org">Ruby</a></li>
+    <li><a href="https://rubyonrails.org/">Ruby on Rails</a></li>
+    <li><a href="https://www.python.org/">Python</a></li>
+    <li><a href="https://flask.palletsprojects.com/">Flask</a></li>
+    <li><a href="https://crystal-lang.org">Crystal</a></li>
 </ul>
 
 <p>If you would like to hire me, please visit my <a href="https://freelancer.com/u/zachiahsawyer">Profile on freelancer.com</a></p>
